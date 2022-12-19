b4edc73fae2409a63f12fe531e125f93ae6d1048
Sergei Dmitriev <iserdmi@gmail.com>
2022-12-19 16:57:16 +0100

    Add ideas list jsx structure

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,32 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <div className="App">
+    <div>
+      <h1>Ideanick</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Idea 1</h2>
+        <p>Description of idea 1...</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>Idea 2</h2>
+        <p>Description of idea 2...</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h2>Idea 3</h2>
+        <p>Description of idea 3...</p>
+      </div>
     </div>
   )
 }
