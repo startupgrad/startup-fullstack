8663277e5c4844ec0cf2d99cbff938e92f7d017b
Sergei Dmitriev <iserdmi@gmail.com>
2022-12-19 17:06:07 +0100

    Get ideas from array

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,19 +1,20 @@
 export const App = () => {
+  const ideas = [
+    { nick: "cool-idea-nick-1", name: "Idea 1", description: "Description of idea 1..." },
+    { nick: "cool-idea-nick-2", name: "Idea 2", description: "Description of idea 2..." },
+    { nick: "cool-idea-nick-3", name: "Idea 3", description: "Description of idea 3..." },
+    { nick: "cool-idea-nick-4", name: "Idea 4", description: "Description of idea 4..." },
+    { nick: "cool-idea-nick-5", name: "Idea 5", description: "Description of idea 5..." },
+  ]
   return (
     <div>
       <h1>Ideanick</h1>
-      <div>
-        <h2>Idea 1</h2>
-        <p>Description of idea 1...</p>
-      </div>
-      <div>
-        <h2>Idea 2</h2>
-        <p>Description of idea 2...</p>
-      </div>
-      <div>
-        <h2>Idea 3</h2>
-        <p>Description of idea 3...</p>
-      </div>
+      {ideas.map((idea) => (
+        <div key={idea.nick}>
+          <h2>{idea.name}</h2>
+          <p>{idea.description}</p>
+        </div>
+      ))}
     </div>
   )
 }
