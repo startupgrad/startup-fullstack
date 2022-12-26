0d01eba9aec3dd9a1f95d5b9f0344eab5a89ada4
Sergei Dmitriev <iserdmi@gmail.com>
2022-12-26 18:42:49 +0100

    Add express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello!')
+import express from 'express'
+
+void (async () => {
+  try {
+    const expressApp = express()
+    expressApp.get('/ping', (req, res) => {
+      res.send('pong')
+    })
+    expressApp.listen(3000, () => {
+      console.info('Listening at http://localhost:3000')
+    })
+  } catch (error) {
+    console.error(error)
+  }
+})()
