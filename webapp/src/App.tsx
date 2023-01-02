5e4fc5598e7a6808d10301a13e7840ae7e376c34
Sergei Dmitriev <iserdmi@gmail.com>
2023-01-02 13:56:12 +0100

    Add ViewIdeaPage

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,12 @@
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
+import { ViewIdeaPage } from './pages/ViewIdeaPage'
 
 export const App = () => {
   return (
     <TrpcProvider>
       <AllIdeasPage />
+      <ViewIdeaPage />
     </TrpcProvider>
   )
 }
---
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -0,0 +1,13 @@
+export const ViewIdeaPage = () => {
+  return (
+    <div>
+      <h1>Idea 1</h1>
+      <p>Description of idea 1...</p>
+      <div>
+        <p>Text paragrph 1 of idea 1...</p>
+        <p>Text paragrph 2 of idea 1...</p>
+        <p>Text paragrph 3 of idea 1...</p>
+      </div>
+    </div>
+  )
+}
