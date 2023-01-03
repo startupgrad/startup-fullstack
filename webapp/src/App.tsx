053bb9c52682d4f5d546db277faad9e1c9f266ff
Sergei Dmitriev <iserdmi@gmail.com>
2023-01-03 11:55:55 +0100

    Add NewIdeaPage

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,6 +3,7 @@ import { Layout } from './components/Layout'
 import * as routes from './lib/routes'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
+import { NewIdeaPage } from './pages/NewIdeaPage'
 import { ViewIdeaPage } from './pages/ViewIdeaPage'
 import './styles/global.scss'
 
@@ -14,6 +15,7 @@ export const App = () => {
           <Route element={<Layout />}>
             <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />} />
             <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+            <Route path={routes.getNewIdeaRoute()} element={<NewIdeaPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
