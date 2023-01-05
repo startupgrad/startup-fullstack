e6fc576a973f0a142c9703b608a8f172107a715e
Sergei Dmitriev <iserdmi@gmail.com>
2023-01-05 13:08:09 +0100

    Add generate index extension

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,10 +1,14 @@
 import { trpc } from '../lib/trpc'
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()} } from '${f.path.split('/').slice(0, -1).join('/')}'`)
 import { getIdea } from './getIdea'
 import { getIdeas } from './getIdeas'
+// @endindex
 
 export const trpcRouter = trpc.router({
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()},`)
   getIdea,
   getIdeas,
+  // @endindex
 })
 
 export type TrpcRouter = typeof trpcRouter
