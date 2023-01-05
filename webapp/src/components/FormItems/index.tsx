0620a77732e602e9a85ea09f88961f16467290d6
Sergei Dmitriev <iserdmi@gmail.com>
2023-01-05 22:43:50 +0100

    Add backend app context, add prisma to getIdeas query

--- webapp/src/components/FormItems/index.tsx
+++ webapp/src/components/FormItems/index.tsx
@@ -1,5 +1,5 @@
-import css from './styles.module.scss'
 import React from 'react'
+import css from './styles.module.scss'
 
 export const FormItems: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return <div className={css.formItems}>{children}</div>
