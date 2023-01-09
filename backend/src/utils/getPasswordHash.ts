7feded81d743f2623105d6241d050a7ceb9cbbcc
Sergei Dmitriev <iserdmi@gmail.com>
2023-01-09 21:34:30 +0100

    Add salt to password hash

--- backend/src/utils/getPasswordHash.ts
+++ backend/src/utils/getPasswordHash.ts
@@ -1,5 +1,6 @@
+import { env } from '../lib/env'
 import crypto from 'crypto'
 
 export const getPasswordHash = (password: string) => {
-  return crypto.createHash('sha256').update(password).digest('hex')
+  return crypto.createHash('sha256').update(`${env.PASSWORD_SALT}${password}`).digest('hex')
 }
